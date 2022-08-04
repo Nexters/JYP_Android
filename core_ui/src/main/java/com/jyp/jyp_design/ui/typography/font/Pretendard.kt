b4d570b8240139ce00677aeaff1eddf830c5a1d2
yunso <cw2cu96@gmail.com>
2022-08-05 00:42:03 +0900

    #9: Pretendard font family 설정

---
+++ core_ui/src/main/java/com/jyp/jyp_design/ui/typography/font/Pretendard.kt
@@ -0,0 +1,14 @@
+package com.jyp.jyp_design.ui.typography.font
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.jyp.jyp_design.R
+
+
+val PretendardFontFamily = FontFamily(
+    Font(R.font.pretendard_regular, FontWeight.Normal),
+    Font(R.font.pretendard_medium, FontWeight.Medium),
+    Font(R.font.pretendard_semi_bold, FontWeight.SemiBold),
+    Font(R.font.pretendard_bold, FontWeight.Bold)
+)
