bffa8bb1743e9e64730b12a17a3941d1fd6e9bd1
yunso <cw2cu96@gmail.com>
2022-08-10 23:34:11 +0900

    #9: 각 텍스트 유형별 enum 통합

--- core_ui/src/main/java/com/jyp/jyp_design/ui/typography/type/TagType.kt
+++
@@ -1,5 +0,0 @@
-package com.jyp.jyp_design.ui.typography.type
-
-enum class TagType {
-    TAG_1, TAG_2
-}
---
+++ core_ui/src/main/java/com/jyp/jyp_design/ui/typography/type/TextType.kt
@@ -0,0 +1,9 @@
+package com.jyp.jyp_design.ui.typography.type
+
+
+enum class TextType {
+    HEADING_1, HEADING_2, HEADING_3,
+    TITLE_1, TITLE_2, TITLE_3, TITLE_4, TITLE_5, TITLE_6,
+    BODY_1, BODY_2, BODY_3, BODY_4,
+    TAG_1, TAG_2
+}
