94f9b2fdfdc94826f6194f9bdc77698562d71701
dean <sodp5@naver.com>
2022-08-13 15:28:34 +0900

    TagType 색상 추가

---
+++ core_ui/src/main/java/com/jyp/jyp_design/ui/tag/TagType.kt
@@ -0,0 +1,62 @@
+package com.jyp.jyp_design.ui.tag
+
+import androidx.compose.ui.graphics.Color
+import com.jyp.jyp_design.resource.JypColors
+
+enum class TagType(
+        defaultBackgroundColor: Color,
+        defaultTextColor: Color,
+        defaultIconColor: Color,
+        defaultIconBackgroundColor: Color,
+        selectedBackgroundColor: Color,
+        selectedTextColor: Color,
+        selectedIconColor: Color,
+        selectedIconBackgroundColor: Color,
+        inactiveBackgroundColor: Color,
+        inactiveTextColor: Color,
+        inactiveIconColor: Color,
+        inactiveIconBackgroundColor: Color,
+) {
+    SOSO(
+            defaultBackgroundColor = JypColors.Tag_white_blue100,
+            defaultTextColor = JypColors.Sub_blue300,
+            defaultIconColor = JypColors.Sub_blue300,
+            defaultIconBackgroundColor = JypColors.Text_white,
+            selectedBackgroundColor = JypColors.Tag_blue100,
+            selectedTextColor = JypColors.Text_white,
+            selectedIconColor = JypColors.Text_white,
+            selectedIconBackgroundColor = JypColors.Sub_blue300,
+            inactiveBackgroundColor = JypColors.Tag_white_grey100,
+            inactiveTextColor = JypColors.Tag_grey200,
+            inactiveIconColor = JypColors.Tag_grey200,
+            inactiveIconBackgroundColor = JypColors.Text_white,
+    ),
+    LIKE(
+            defaultBackgroundColor = JypColors.Tag_white_red100,
+            defaultTextColor = JypColors.Tag_red300,
+            defaultIconColor = JypColors.Tag_red300,
+            defaultIconBackgroundColor = JypColors.Text_white,
+            selectedBackgroundColor = JypColors.Tag_red200,
+            selectedTextColor = JypColors.Text_white,
+            selectedIconColor = JypColors.Text_white,
+            selectedIconBackgroundColor = JypColors.Tag_red300,
+            inactiveBackgroundColor = JypColors.Tag_white_grey100,
+            inactiveTextColor = JypColors.Tag_grey200,
+            inactiveIconColor = JypColors.Tag_grey200,
+            inactiveIconBackgroundColor = JypColors.Text_white,
+    ),
+    DISLIKE(
+            defaultBackgroundColor = JypColors.Tag_white_blue100,
+            defaultTextColor = JypColors.Tag_orange300,
+            defaultIconColor = JypColors.Tag_orange300,
+            defaultIconBackgroundColor = JypColors.Text_white,
+            selectedBackgroundColor = JypColors.Tag_blue100,
+            selectedTextColor = JypColors.Text_white,
+            selectedIconColor = JypColors.Tag_brown200,
+            selectedIconBackgroundColor = JypColors.Tag_yellow100,
+            inactiveBackgroundColor = JypColors.Tag_white_grey100,
+            inactiveTextColor = JypColors.Tag_grey200,
+            inactiveIconColor = JypColors.Tag_grey200,
+            inactiveIconBackgroundColor = JypColors.Text_white,
+    ),
+}
