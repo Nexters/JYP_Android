c6f758ec4532d578ad56b3e4a3f2621adacf4d7b
yunso <cw2cu96@gmail.com>
2022-08-13 20:47:36 +0900

    #16: ButtonColorSetType enum 생성

--- core_ui/src/main/java/com/jyp/jyp_design/ui/button/ButtonType.kt
+++ core_ui/src/main/java/com/jyp/jyp_design/ui/button/ButtonType.kt
@@ -1,5 +1,7 @@
 package com.jyp.jyp_design.ui.button
 
+import androidx.compose.ui.graphics.Color
+import com.jyp.jyp_design.resource.JypColors
 import com.jyp.jyp_design.ui.typography.type.TextType
 
 
@@ -18,5 +20,27 @@ enum class ButtonType(
     THIN(
         verticalPadding = 8,
         textType = TextType.BODY_1
+    )
+}
+
+enum class ButtonColorSetType(
+    val backgroundColor: Color,
+    val textColor: Color
+) {
+    PINK(
+        backgroundColor = JypColors.Pink,
+        textColor = JypColors.Text_white
+    ),
+    BLACK(
+        backgroundColor = JypColors.Sub_black,
+        textColor = JypColors.Text_white
+    ),
+    YELLOW(
+        backgroundColor = JypColors.Yellow,
+        textColor = JypColors.Text75
+    ),
+    GRAY(
+        backgroundColor = JypColors.Tag_white_grey100,
+        textColor = JypColors.Text40
     )
 }
