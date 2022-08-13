0b5d4bb97a60adcfd6503e6e3346abff440f1bb6
dean <sodp5@naver.com>
2022-08-13 21:39:23 +0900

    #33 컨텐츠 탭 추가

--- feature_planner/src/main/java/com/jyp/feature_planner/presentation/planner/PlannerScreen.kt
+++ feature_planner/src/main/java/com/jyp/feature_planner/presentation/planner/PlannerScreen.kt
@@ -1,16 +1,23 @@
 package com.jyp.feature_planner.presentation.planner
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.*
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.*
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.jyp.feature_planner.R
 import com.jyp.jyp_design.resource.JypColors
 import com.jyp.jyp_design.ui.gnb.GlobalNavigationBar
@@ -22,6 +29,9 @@ import com.jyp.jyp_design.ui.typography.type.TextType
 @Composable
 internal fun PlannerScreen() {
     val scaffoldState = rememberBackdropScaffoldState(BackdropValue.Revealed)
+    var selectedTabPosition by remember {
+        mutableStateOf(0)
+    }
 
     BackdropScaffold(
             scaffoldState = scaffoldState,
@@ -36,9 +46,17 @@ internal fun PlannerScreen() {
                 PlannerBackLayer()
             },
             frontLayerContent = {
-                PlannerContent()
+                PlannerContent(
+                        tabTitles = listOf(
+                                stringResource(id = R.string.planner_tab_forum),
+                                stringResource(id = R.string.planner_tab_piki),
+                        ),
+                        selectedTabPosition = selectedTabPosition,
+                        tabSelected = { selectedTabPosition = it }
+                )
             },
-            backLayerBackgroundColor = JypColors.Background_grey300
+            backLayerBackgroundColor = JypColors.Background_grey300,
+            frontLayerScrimColor = Color.Transparent,
     )
 }
 
@@ -59,7 +77,7 @@ private fun PlannerBackLayer() {
         Button(
                 modifier = Modifier
                         .height(40.dp),
-                onClick = {  },
+                onClick = { },
                 colors = ButtonDefaults.buttonColors(backgroundColor = JypColors.Pink),
                 contentPadding = PaddingValues(0.dp)
         ) {
@@ -82,7 +100,11 @@ private fun PlannerBackLayer() {
 }
 
 @Composable
-private fun PlannerContent() {
+private fun PlannerContent(
+        tabTitles: List<String>,
+        selectedTabPosition: Int,
+        tabSelected: (position: Int) -> Unit,
+) {
     Column(
             modifier = Modifier
                     .fillMaxSize()
@@ -94,7 +116,71 @@ private fun PlannerContent() {
                     )
                     .background(JypColors.Background_white100),
     ) {
+        PlannerContentTab(
+                tabTitles,
+                selectedTabPosition = selectedTabPosition,
+                tabSelected = tabSelected,
+        )
+    }
+}
 
+@Composable
+private fun PlannerContentTab(
+        tabTitles: List<String>,
+        selectedTabPosition: Int,
+        tabSelected: (position: Int) -> Unit,
+) {
+    Row(
+            modifier = Modifier
+                    .fillMaxWidth()
+                    .drawBehind {
+                        drawLine(
+                                color = JypColors.Background_white200,
+                                start = Offset(0f, size.height + 1.5.dp.toPx()),
+                                end = Offset(size.width, size.height + 1.5.dp.toPx()),
+                                strokeWidth = 2.dp.toPx()
+                        )
+                    }
+                    .padding(top = 24.dp, start = 24.dp, end = 20.dp)
+    ) {
+        tabTitles.forEachIndexed { index, tabTitle ->
+            val selected = selectedTabPosition == index
+            Text(
+                    modifier = Modifier
+                            .let { modifier ->
+                                if (index > 0) {
+                                    modifier.padding(start = 28.dp)
+                                } else {
+                                    modifier
+                                }
+                            }
+                            .drawWithContent {
+                                drawContent()
+                                if (selected) {
+                                    drawRoundRect(
+                                            color = JypColors.Text80,
+                                            topLeft = Offset(0f, size.height + 3.dp.toPx()),
+                                            cornerRadius = CornerRadius(x = 16.dp.toPx(), y = 16.dp.toPx()),
+                                    )
+                                }
+                            }
+                            .clickable(
+                                    interactionSource = remember { MutableInteractionSource() },
+                                    indication = rememberRipple()
+                            ) {
+                                tabSelected.invoke(index)
+                            }
+                            .padding(vertical = 6.dp),
+                    text = tabTitle,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    color = if (selected) {
+                        JypColors.Text80
+                    } else {
+                        JypColors.Text40
+                    },
+            )
+        }
     }
 }
 
