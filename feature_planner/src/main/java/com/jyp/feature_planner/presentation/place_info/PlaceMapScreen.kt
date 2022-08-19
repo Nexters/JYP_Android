8ed03067c3818d7fb2cd1d43a645fb096add8bd7
yunso <yxnsx@munpia.com>
2022-08-19 21:40:40 +0900

    #46: PlaceMapScreen.kt 파일의 UI 구성 구체화

--- feature_planner/src/main/java/com/jyp/feature_planner/presentation/place_info/PlaceMapScreen.kt
+++ feature_planner/src/main/java/com/jyp/feature_planner/presentation/place_info/PlaceMapScreen.kt
@@ -2,22 +2,20 @@ package com.jyp.feature_planner.presentation.place_info
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.VerticalAlignmentLine
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.zIndex
 import com.jyp.feature_planner.R
-import com.jyp.feature_planner.presentation.planner.PlannerScreen
 import com.jyp.jyp_design.resource.JypColors
 import com.jyp.jyp_design.ui.button.ButtonColorSetType
 import com.jyp.jyp_design.ui.button.ButtonType
@@ -38,7 +36,7 @@ internal fun PlaceMapScreen() {
             verticalAlignment = Alignment.CenterVertically
         ) {
             IconButton(
-                onClick = {}
+                onClick = {} // Todo - Set click event for back button.
             ) {
                 Icon(
                     painter = painterResource(com.jyp.jyp_design.R.drawable.icon_left_arrow),
@@ -50,18 +48,40 @@ internal fun PlaceMapScreen() {
                     tint = JypColors.Sub_black
                 )
             }
-            Box(
+            Row(
                 modifier = Modifier
+                    .padding(end = 24.dp)
+                    .padding(vertical = 10.dp)
                     .fillMaxWidth()
-                    .padding(
-                        start = 8.dp,
-                        end = 24.dp,
+                    .wrapContentHeight()
+                    .background(
+                        color = JypColors.Background_white200,
+                        shape = RoundedCornerShape(6.dp)
+                    )
+                    .border(
+                        width = 1.dp,
+                        color = JypColors.Border_grey,
+                        shape = RoundedCornerShape(6.dp)
                     )
             ) {
                 JypText(
                     text = "아르떼 뮤지엄",
                     type = TextType.TITLE_3,
+                    modifier = Modifier
+                        .padding(vertical = 6.dp)
+                        .padding(start = 12.dp)
+                        .weight(weight = 1f)
+                    ,
                     color = JypColors.Text80
+                )
+                Spacer(modifier = Modifier.size(12.dp))
+                Image(
+                    painter = painterResource(id = com.jyp.jyp_design.R.drawable.ic_text_delete),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .padding(vertical = 8.dp)
+                        .padding(end = 12.dp)
+                        .wrapContentSize()
                 )
             }
         }
@@ -104,36 +124,34 @@ internal fun PlaceMapScreen() {
                         .padding(horizontal = 24.dp),
                     color = JypColors.Text40
                 )
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(bottom = 20.dp)
-                        .padding(horizontal = 24.dp),
-                    contentAlignment = Alignment.TopEnd
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End,
+                    verticalAlignment = Alignment.Top,
                 ) {
-                    Row(
+                    Button(
+                        onClick = {}, // Todo - Set click event for info button.
                         modifier = Modifier
                             .wrapContentSize()
-                            .background(
-                                color = JypColors.Background_white100,
-                                shape = RoundedCornerShape(8.dp)
-                            )
-                            .shadow(
-                                elevation = 3.dp,
-                                shape = RoundedCornerShape(8.dp),
-                                ambientColor = JypColors.Black20.copy(alpha = 0.001f)
-                            )
-                            .zIndex(1f)
-                            .padding(8.dp)
+                            .padding(bottom = 20.dp)
+                            .padding(horizontal = 24.dp),
+                        enabled = true,
+                        shape = RoundedCornerShape(8.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            backgroundColor = JypColors.Background_white100
+                        ),
+                        contentPadding = PaddingValues(horizontal = 8.dp)
                     ) {
                         Icon(
                             painter = painterResource(id = R.drawable.icon_eyes),
-                            contentDescription = null
+                            contentDescription = null,
+                            modifier = Modifier.size(ButtonDefaults.IconSize)
                         )
                         JypText(
                             text = "정보 보기",
                             type = TextType.BODY_1,
-                            color = JypColors.Text90
+                            modifier = Modifier.padding(all = 8.dp),
+                            color = JypColors.Text90,
                         )
                     }
                 }
@@ -144,6 +162,7 @@ internal fun PlaceMapScreen() {
                         .fillMaxWidth()
                         .padding(horizontal = 24.dp)
                         .padding(bottom = 28.dp),
+                    enabled = true,
                     buttonColorSet = ButtonColorSetType.PINK
                 )
             }
