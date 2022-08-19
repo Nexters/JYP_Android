30ec9f32728abd290f28f51d4445a3fc7f6fa220
yxnsx <yxnsx.dev@gmail.com>
2022-08-20 07:27:42 +0900

    #46: PlaceInfoActivity.kt 파일과 PlaceInfoScreen.kt 생성 및 레이아웃 리소스 설정

---
+++ feature_planner/src/main/java/com/jyp/feature_planner/presentation/place_info/PlaceInfoActivity.kt
@@ -0,0 +1,23 @@
+package com.jyp.feature_planner.presentation.place_info
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class PlaceInfoActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Screen()
+        }
+    }
+}
+
+@Composable
+private fun Screen() {
+    PlaceInfoScreen()
+}
