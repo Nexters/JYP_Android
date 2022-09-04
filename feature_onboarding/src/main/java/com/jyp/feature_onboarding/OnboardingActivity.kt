dca4038d755c447ee5c26f316dd8ee85901d1d8d
yunso <cw2cu96@gmail.com>
2022-09-04 09:48:59 +0900

    #25: OnboardingActivity.kt 파일에 OnboardingContent 설정

--- feature_onboarding/src/main/java/com/jyp/feature_onboarding/OnboardingActivity.kt
+++ feature_onboarding/src/main/java/com/jyp/feature_onboarding/OnboardingActivity.kt
@@ -3,13 +3,19 @@ package com.jyp.feature_onboarding
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
 
 
 class OnboardingActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            Screen()
         }
     }
+}
+
+@Composable
+private fun Screen() {
+    OnboardingContent()
 }
