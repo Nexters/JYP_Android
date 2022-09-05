b2a309a5c2247c5f1c12db2c5a437fcbc0dbc72e
yunso <cw2cu96@gmail.com>
2022-09-05 10:04:39 +0900

    #30: QuestionActivity.kt 파일에 viewModel 설정 추가

--- feature_question/src/main/java/com/jyp/feature_question/QuestionActivity.kt
+++ feature_question/src/main/java/com/jyp/feature_question/QuestionActivity.kt
@@ -3,6 +3,7 @@ package com.jyp.feature_question
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.runtime.Composable
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -10,16 +11,18 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class QuestionActivity : ComponentActivity() {
 
+    private val viewModel: QuestionViewModel by viewModels()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Screen()
+            Screen(viewModel)
         }
     }
 }
 
 @Composable
-private fun Screen() {
-    QuestionScreen()
+private fun Screen(viewModel: QuestionViewModel) {
+    QuestionScreen(viewModel = viewModel)
 }
