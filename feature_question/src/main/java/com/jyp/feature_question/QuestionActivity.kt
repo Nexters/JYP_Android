b505eff13cdf9af6d3d0ebbbb35ff8593182ee60
yxnsx <yxnsx.dev@gmail.com>
2022-09-10 00:35:58 +0900

    #30: QuestionActivity.kt 파일의 screen 매개변수로 onQuestionOptionSelected 추가

--- feature_question/src/main/java/com/jyp/feature_question/QuestionActivity.kt
+++ feature_question/src/main/java/com/jyp/feature_question/QuestionActivity.kt
@@ -1,10 +1,14 @@
 package com.jyp.feature_question
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import com.jyp.main.presentation.MainActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -17,12 +21,29 @@ class QuestionActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Screen(viewModel)
+            Screen(
+                viewModel = viewModel,
+                onQuestionFinished = {
+                    startActivity(Intent(this, MainActivity::class.java))
+                }
+            )
         }
     }
 }
 
 @Composable
-private fun Screen(viewModel: QuestionViewModel) {
-    QuestionScreen(viewModel = viewModel)
+private fun Screen(
+    viewModel: QuestionViewModel,
+    onQuestionFinished: () -> Unit
+) {
+    val selectedQuestionOptions: MutableList<Int?>
+            by viewModel.selectedQuestionOptions.observeAsState(mutableListOf())
+
+    QuestionScreen(
+        selectedQuestionOptions = selectedQuestionOptions,
+        onQuestionOptionSelected = { index: Int, selectedOption: Int ->
+            viewModel.setSelectedQuestionOptions(index, selectedOption)
+        },
+        onQuestionFinished = onQuestionFinished
+    )
 }
