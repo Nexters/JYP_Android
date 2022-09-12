f97b4d86deaa19107b3e3ba083ccc434400547ff
yxnsx <yxnsx.dev@gmail.com>
2022-09-12 23:38:00 +0900

    #46: PlaceMapActivity 생성

---
+++ feature_add_place/src/main/java/com/jyp/feature_add_place/PlaceMapActivity.kt
@@ -0,0 +1,11 @@
+package com.jyp.feature_add_place
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlaceMapActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_place_map)
+    }
+}
