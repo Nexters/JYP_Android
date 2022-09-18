29e4f824421e9e4e4fcb2da0dc62ab5509b80a94
yxnsx <yxnsx.dev@gmail.com>
2022-09-19 03:35:48 +0900

    #31: SearchPlaceResponseModelMapper.kt 파일 생성

---
+++ feature_add_place/src/main/java/com/jyp/feature_add_place/data/mapper/SearchPlaceResponseModelMapper.kt
@@ -0,0 +1,14 @@
+package com.jyp.feature_add_place.data.mapper
+
+import com.jyp.core.search_place.domain.model.SearchPlaceResponseModel
+import com.jyp.feature_add_place.data.model.SearchPlaceResultModel
+
+
+fun SearchPlaceResponseModel.Place.toDataModelList() = SearchPlaceResultModel(
+    name = this.place_name,
+    address = this.address_name,
+    category = this.category_name,
+    x = this.x.toDouble(),
+    y = this.y.toDouble(),
+    infoUrl = this.place_url
+)
