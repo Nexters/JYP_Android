a87443ffd47f2007b2fd1504ed916fb51e6fac52
yxnsx <yxnsx.dev@gmail.com>
2022-09-20 23:39:11 +0900

    #31: API 응답으로 사용할 파일의 이름 변경 및 필드 구체화 (ApiResponse.kt -> UiState.kt)

--- core_network/src/main/java/com/jyp/core/ApiResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.jyp.core
-
-sealed class ApiResponse {
-    object Loading : ApiResponse()
-    data class Success<out T>(val data: T) : ApiResponse()
-    data class Failure(val message: String) : ApiResponse()
-}
