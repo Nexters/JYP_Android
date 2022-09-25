6a8c385c4f769c1e8d58e52d939d9285d64a6797
yxnsx <yxnsx.dev@gmail.com>
2022-09-26 00:10:47 +0900

    #31: core_network 모듈 내 SearchPlaceApi.kt 파일 메소드의 쿼리 파라미터 수정

--- core_network/src/main/java/com/jyp/core/search_place/domain/SearchPlaceApi.kt
+++ core_network/src/main/java/com/jyp/core/search_place/domain/SearchPlaceApi.kt
@@ -8,8 +8,6 @@ import retrofit2.http.Query
 interface SearchPlaceApi {
     @GET("/v2/local/search/keyword.json")
     suspend fun getSearchPlaceResult(
-        @Query("query") query: String,
-        @Query("x") x: String = "129.163634",
-        @Query("y") y: String = "35.163177"
+        @Query("query") query: String
     ): SearchPlaceResponseModel
 }
