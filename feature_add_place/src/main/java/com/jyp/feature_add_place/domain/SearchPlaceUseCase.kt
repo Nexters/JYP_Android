144ac42467c761034650606e38972ba4234efa01
yxnsx <yxnsx.dev@gmail.com>
2022-09-26 00:12:34 +0900

    #31: feature_add_place 모듈 내 SearchPlaceUseCase.kt 파일 메소드의 파라미터 삭제

--- feature_add_place/src/main/java/com/jyp/feature_add_place/domain/SearchPlaceUseCase.kt
+++ feature_add_place/src/main/java/com/jyp/feature_add_place/domain/SearchPlaceUseCase.kt
@@ -6,28 +6,29 @@ import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.TimeoutCancellationException
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import javax.inject.Inject
 
 
-fun interface GetSearchPlaceUseCase : (String) -> Flow<Result<List<SearchPlaceResultModel>>>
+class GetSearchPlaceUseCase @Inject constructor(
+    private val searchPlaceRepositoryInterface: SearchPlaceRepositoryInterface
+) {
+    fun getSearchPlaceResult(
+        placeName: String
+    ): Flow<Result<List<SearchPlaceResultModel>>> {
+        return searchPlaceRepositoryInterface
+            .getSearchPlaceResult(placeName)
+            .map { resultOf { it } }
+    }
 
-fun getSearchPlaceResult(
-    searchPlaceRepositoryInterface: SearchPlaceRepositoryInterface,
-    placeName: String
-): Flow<Result<List<SearchPlaceResultModel>>> {
-    Log.d("TAG", "UseCase - getSearchPlaceResult: $placeName")
-    return searchPlaceRepositoryInterface
-        .getSearchPlaceResult(placeName)
-        .map { resultOf { it } }
-}
-
-inline fun <R> resultOf(block: () -> R): Result<R> {
-    return try {
-        Result.success(block())
-    } catch (t: TimeoutCancellationException) {
-        Result.failure(t)
-    } catch (c: CancellationException) {
-        throw c
-    } catch (e: Exception) {
-        Result.failure(e)
+    private inline fun <R> resultOf(block: () -> R): Result<R> {
+        return try {
+            Result.success(block())
+        } catch (t: TimeoutCancellationException) {
+            Result.failure(t)
+        } catch (c: CancellationException) {
+            throw c
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
     }
 }
