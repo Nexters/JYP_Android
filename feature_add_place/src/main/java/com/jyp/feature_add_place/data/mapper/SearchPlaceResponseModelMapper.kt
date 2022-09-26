7ded02538743b42d30eadf571ea91e6e0b3307eb
yxnsx <yxnsx.dev@gmail.com>
2022-09-26 20:51:02 +0900

    #31: SearchPlaceResponseModelMapper.kt 파일의 category 값 수정

--- feature_add_place/src/main/java/com/jyp/feature_add_place/data/mapper/SearchPlaceResponseModelMapper.kt
+++ feature_add_place/src/main/java/com/jyp/feature_add_place/data/mapper/SearchPlaceResponseModelMapper.kt
@@ -7,7 +7,7 @@ import com.jyp.feature_add_place.data.model.SearchPlaceResultModel
 fun SearchPlaceResponseModel.Place.toDataModelList() = SearchPlaceResultModel(
     name = this.place_name,
     address = this.address_name,
-    category = this.category_name,
+    category = this.category_group_name,
     x = this.x.toDouble(),
     y = this.y.toDouble(),
     infoUrl = this.place_url
