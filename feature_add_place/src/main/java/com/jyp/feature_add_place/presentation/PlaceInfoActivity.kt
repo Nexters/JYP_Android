3a34914e71eaf2ec7423a917c37ac0cb8c57ce98
yxnsx <yxnsx.dev@gmail.com>
2022-09-26 23:54:32 +0900

    #31: PlaceInfoActivity 파일과 PlaceInfoScreen.kt 파일의 패키지 변경

--- feature_add_place/src/main/java/com/jyp/feature_add_place/presentation/PlaceInfoActivity.kt
+++ feature_add_place/src/main/java/com/jyp/feature_add_place/presentation/PlaceInfoActivity.kt
@@ -1,9 +1,10 @@
-package com.jyp.feature_add_place
+package com.jyp.feature_add_place.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
+import com.jyp.feature_add_place.PlaceInfoScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 
