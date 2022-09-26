ebda56189b5ee6d53d1756f1562390416294292a
yxnsx <yxnsx.dev@gmail.com>
2022-09-26 20:54:51 +0900

    #31: 장소 카테고리 정보 변환을 위한 JourneyPikiPlaceCategoryEnum 및 Extensions 파일 생성

--- feature_add_place/src/main/java/com/jyp/feature_add_place/util/Extensions.kt
+++ feature_add_place/src/main/java/com/jyp/feature_add_place/util/Extensions.kt
@@ -9,6 +9,27 @@ import androidx.annotation.RequiresApi
 import com.jyp.feature_add_place.R
 
 
+fun String.getJourneyPikiPlaceCategoryEnum(): JourneyPikiPlaceCategoryEnum {
+    return when (this) {
+        "대형마트" -> JourneyPikiPlaceCategoryEnum.MART
+        "편의점" -> JourneyPikiPlaceCategoryEnum.CONVENIENCE_STORE
+        "학교" -> JourneyPikiPlaceCategoryEnum.SCHOOL
+        "지하철역" -> JourneyPikiPlaceCategoryEnum.TRANSPORTATION
+        "문화시설" -> JourneyPikiPlaceCategoryEnum.CULTURAL_INSTITUTION
+        "공공기관" -> JourneyPikiPlaceCategoryEnum.PUBLIC_INSTITUTION
+        "관광명소" -> JourneyPikiPlaceCategoryEnum.TOUR_SPOT
+        "숙박" -> JourneyPikiPlaceCategoryEnum.LODGING
+        "음식점" -> JourneyPikiPlaceCategoryEnum.RESTAURANT
+        "카페" -> JourneyPikiPlaceCategoryEnum.CAFE
+        "병원" -> JourneyPikiPlaceCategoryEnum.HOSPITAL
+        "약국" -> JourneyPikiPlaceCategoryEnum.PHARMACY
+        "은행" -> JourneyPikiPlaceCategoryEnum.BANK
+        "충전소" -> JourneyPikiPlaceCategoryEnum.CHARGING_ZONE
+        "주차장" -> JourneyPikiPlaceCategoryEnum.PARKING_LOT
+        else -> JourneyPikiPlaceCategoryEnum.ETC
+    }
+}
+
 @RequiresApi(Build.VERSION_CODES.M)
 inline fun Context.checkNetworkStatus(
     isActivated: () -> Unit,
