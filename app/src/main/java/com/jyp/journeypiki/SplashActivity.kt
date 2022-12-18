75e60be99f97be104a6a3d592d966a7057a604fa
dean <sodp5@naver.com>
2022-12-18 23:15:30 +0900

    #68 디버깅용 코드 되돌리기

--- app/src/main/java/com/jyp/journeypiki/SplashActivity.kt
+++ app/src/main/java/com/jyp/journeypiki/SplashActivity.kt
@@ -8,8 +8,6 @@ import androidx.activity.compose.setContent
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
 import androidx.core.os.postDelayed
-import com.jyp.feature_planner.presentation.create_planner.CreatePlannerActivity
-import com.jyp.feature_planner.presentation.create_planner.model.CreatePlannerStep
 import com.jyp.main.presentation.MainActivity
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -25,16 +23,8 @@ class SplashActivity : ComponentActivity() {
 
         Handler(Looper.getMainLooper()).postDelayed(500L) {
             // TODO : onboarding 화면으로 이동하기
-//            startActivity(Intent(this, CreatePlannerActivity::class.java).apply {
-//                putExtra(CreatePlannerActivity.EXTRA_CREATE_PLANNER_STEP, CreatePlannerStep.TASTE)
-//            })
+            startActivity(Intent(this, MainActivity::class.java))
         }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        startActivity(Intent(this, CreatePlannerActivity::class.java).apply {
-        })
     }
 }
 
