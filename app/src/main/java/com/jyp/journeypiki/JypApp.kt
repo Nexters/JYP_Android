9f2da58fd5de62c26ee957ef457a6e968f5b4583
yxnsx <yxnsx.dev@gmail.com>
2023-02-09 22:58:45 +0900

    #60: MySharedPreferences.kt 파일 삭제

--- app/src/main/java/com/jyp/journeypiki/JypApp.kt
+++ app/src/main/java/com/jyp/journeypiki/JypApp.kt
@@ -1,7 +1,6 @@
 package com.jyp.journeypiki
 
 import android.app.Application
-import com.jyp.feature_sign_in.util.MySharedPreferences
 import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 
@@ -11,7 +10,6 @@ class JypApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        MySharedPreferences.init(applicationContext)
         KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_APP_KEY)
     }
 }
