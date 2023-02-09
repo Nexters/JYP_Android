48accc3d40a4a3a2269c39d1cb0263d0431473f1
yxnsx <yxnsx.dev@gmail.com>
2023-02-09 22:36:40 +0900

    #60: Extensions.kt 파일의 인텐트 확장함수 수정

--- feature_sign_in/src/main/java/com/jyp/feature_sign_in/util/Extensions.kt
+++ feature_sign_in/src/main/java/com/jyp/feature_sign_in/util/Extensions.kt
@@ -1,15 +1,14 @@
 package com.jyp.feature_sign_in.util
 
-import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
 import android.widget.Toast
 
 
 private var toast: Toast? = null
 fun Context.showToast(message: Any) {
     toast?.cancel()
-
     toast = when (message) {
         is Int -> Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT)
         is String -> Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT)
@@ -18,10 +17,9 @@ fun Context.showToast(message: Any) {
     toast?.show()
 }
 
-fun Activity.setIntentTo(activity: Activity, extras: Map<String, String>? = null) {
-    val intent = Intent(this, activity::class.java)
-    extras?.forEach { it ->
-        intent.putExtra(it.key, it.value)
+fun <T> Context.setIntentTo(it: Class<T>, extras: Bundle.() -> Unit = {}) {
+    Intent(this, it).apply {
+        putExtras(Bundle().apply(extras))
+        startActivity(this)
     }
-    startActivity(intent)
 }
