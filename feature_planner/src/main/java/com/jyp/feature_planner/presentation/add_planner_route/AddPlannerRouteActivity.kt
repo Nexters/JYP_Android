68c7ccf99a349bf54517e45e35dcfd4fd5f2b8d9
dean <sodp5@naver.com>
2023-02-11 18:46:29 +0900

    #82 여행 계획 수정 시 화면 닫히도록 변경

--- feature_planner/src/main/java/com/jyp/feature_planner/presentation/add_planner_route/AddPlannerRouteActivity.kt
+++ feature_planner/src/main/java/com/jyp/feature_planner/presentation/add_planner_route/AddPlannerRouteActivity.kt
@@ -1,12 +1,15 @@
 package com.jyp.feature_planner.presentation.add_planner_route
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.jyp.jyp_design.ui.gnb.GlobalNavigationBarColor
@@ -44,20 +47,26 @@ private fun Screen(
     val pikis by viewModel.pikis.collectAsState()
 
     GlobalNavigationBarLayout(
-            color = GlobalNavigationBarColor.WHITE,
-            title = "Day1",
-            titleFontWeight = FontWeight.SemiBold,
-            titleSize = 20.sp,
-            description = "7월 18일",
-            backAction = {},
-            activeBack = true,
+        color = GlobalNavigationBarColor.WHITE,
+        title = "Day1",
+        titleFontWeight = FontWeight.SemiBold,
+        titleSize = 20.sp,
+        description = "7월 18일",
+        backAction = {},
+        activeBack = true,
     ) {
+        val activity = LocalContext.current as? Activity
+
         AddPlannerRouteScreen(
             pikmis = pikmis,
             pikis = pikis,
             onSelectPikme = viewModel::addPiki,
             onRemovePiki = viewModel::removePiki,
-            onSubmitPikis = viewModel::setPikis,
+            onSubmitPikis = {
+                viewModel.setPikis()
+
+                activity?.finish()
+            },
         )
     }
 }
