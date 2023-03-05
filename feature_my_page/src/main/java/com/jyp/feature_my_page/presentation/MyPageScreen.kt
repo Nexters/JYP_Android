80e4c592686aa8fa00d6bb6ac20a36ff20a2ca97
yxnsx <yxnsx.dev@gmail.com>
2023-03-05 20:18:31 +0900

    #90: 마이 페이지 UI 변경 반영

--- feature_my_page/src/main/java/com/jyp/feature_my_page/presentation/MyPageScreen.kt
+++ feature_my_page/src/main/java/com/jyp/feature_my_page/presentation/MyPageScreen.kt
@@ -1,14 +1,18 @@
 package com.jyp.feature_my_page.presentation
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.jyp.feature_my_page.BuildConfig
 import com.jyp.jyp_design.resource.JypColors
 import com.jyp.jyp_design.ui.avatar.Avatar
 import com.jyp.jyp_design.ui.text.*
@@ -16,149 +20,195 @@ import com.jyp.jyp_design.ui.typography.type.*
 
 @Composable
 fun MyPageScreen(
-        journeyPropensity: String,
+    profileImagePath: String,
+    personality: String,
+    userName: String,
+    onClickAppInfo: () -> Unit,
+    onClickSignOut: () -> Unit,
+    onClickWithdrawal: () -> Unit
 ) {
-    Box(
-            modifier = Modifier
-                    .fillMaxSize()
-                    .background(JypColors.Background_white100),
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(JypColors.Background_white100),
     ) {
-        Box(
-                modifier = Modifier
-                        .fillMaxWidth()
-                        .height(197.dp)
-                        .background(JypColors.Background_white200),
+        Spacer(modifier = Modifier.size(30.dp))
+        Profile(
+            modifier = Modifier.fillMaxWidth(),
+            profileImagePath = profileImagePath,
+            personality = personality,
+            userName = userName
         )
-        Column(
-                modifier = Modifier
-                        .padding(horizontal = 24.dp)
-                        .fillMaxSize(),
-        ) {
-            JypText(
-                    modifier = Modifier
-                            .padding(top = 8.dp),
-                    text = journeyPropensity,
-                    type = TextType.HEADING_1,
-                    textAlign = TextAlign.Start,
-                    color = JypColors.Text90,
-            )
-
-            Spacer(modifier = Modifier.size(100.dp))
-
-            Profile()
-
-            Spacer(modifier = Modifier.size(40.dp))
-
-            Menu()
-        }
+        Menu(
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(1f, true)
+                .background(color = JypColors.Background_white200)
+                .padding(
+                    horizontal = 24.dp,
+                    vertical = 28.dp
+                ),
+            onClickAppInfo = onClickAppInfo,
+            onClickSignOut = onClickSignOut,
+            onClickWithdrawal = onClickWithdrawal
+        )
     }
 }
 
 @Composable
-private fun Profile() {
+private fun Profile(
+    modifier: Modifier,
+    profileImagePath: String,
+    personality: String,
+    userName: String
+) {
     Column(
-            modifier = Modifier.fillMaxWidth(),
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Avatar(
-                modifier = Modifier
-                        .align(Alignment.CenterHorizontally),
-                profileImageUrl = "https://file.mk.co.kr/meet/neds/2021/04/image_readtop_2021_330747_16177500644599916.jpg",
-                width = 88.dp,
-                height = 88.dp,
-                showBorder = false,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally),
+            profileImageUrl = profileImagePath,
+            width = 88.dp,
+            height = 88.dp,
+            showBorder = false,
         )
-
-        Spacer(modifier = Modifier.size(9.dp))
-
+        Spacer(modifier = Modifier.size(12.dp))
         JypText(
-                modifier = Modifier
-                        .align(Alignment.CenterHorizontally),
-                text = "박진영",
-                type = TextType.TITLE_2,
-                textAlign = TextAlign.Start,
-                color = JypColors.Text90,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally),
+            text = personality,
+            type = TextType.HEADING_2,
+            textAlign = TextAlign.Center,
+            color = JypColors.Text90,
         )
+        Spacer(modifier = Modifier.size(2.dp))
+        JypText(
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally),
+            text = userName,
+            type = TextType.BODY_1,
+            textAlign = TextAlign.Center,
+            color = JypColors.Text80,
+        )
+        Spacer(modifier = Modifier.size(30.dp))
     }
 }
 
 @Composable
-internal fun Menu() {
-    Column {
+internal fun Menu(
+    modifier: Modifier,
+    onClickAppInfo: () -> Unit,
+    onClickSignOut: () -> Unit,
+    onClickWithdrawal: () -> Unit
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+
+    Column(
+        modifier = modifier
+    ) {
         JypText(
-                modifier = Modifier.padding(start = 14.dp),
-                text = "공지사항",
-                type = TextType.TITLE_6,
-                textAlign = TextAlign.Start,
-                color = JypColors.Text80,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    color = JypColors.Background_white100,
+                    shape = RoundedCornerShape(10.dp)
+                )
+                .padding(
+                    horizontal = 20.dp,
+                    vertical = 23.dp
+                )
+                .clickable(
+                    interactionSource = interactionSource,
+                    indication = null,
+                ) { onClickAppInfo() },
+            text = "앱 소식 및 설명서",
+            type = TextType.TITLE_5,
+            textAlign = TextAlign.Start,
+            color = JypColors.Text80,
         )
-
         Spacer(modifier = Modifier.size(12.dp))
-        Divider()
-        Spacer(modifier = Modifier.size(12.dp))
-
-        JypText(
-                modifier = Modifier.padding(start = 14.dp),
-                text = "이용약관",
-                type = TextType.TITLE_6,
-                textAlign = TextAlign.Start,
-                color = JypColors.Text80,
-        )
-
-        Spacer(modifier = Modifier.size(12.dp))
-        Divider()
-        Spacer(modifier = Modifier.size(12.dp))
-
         Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    color = JypColors.Background_white100,
+                    shape = RoundedCornerShape(10.dp)
+                )
+                .padding(
+                    horizontal = 20.dp,
+                    vertical = 23.dp
+                )
         ) {
             JypText(
-                    modifier = Modifier.padding(start = 14.dp),
-                    text = "버전정보",
-                    type = TextType.TITLE_6,
-                    textAlign = TextAlign.Start,
-                    color = JypColors.Text80,
-            )
-
-            JypText(
-                    modifier = Modifier.padding(end = 14.dp),
-                    text = "1.0",
-                    type = TextType.TITLE_6,
-                    textAlign = TextAlign.Start,
-                    color = JypColors.Text80,
-            )
-        }
-
-
-
-        Spacer(modifier = Modifier.size(12.dp))
-        Divider()
-        Spacer(modifier = Modifier.size(12.dp))
-
-        JypText(
-                modifier = Modifier.padding(start = 14.dp),
-                text = "로그아웃",
-                type = TextType.TITLE_6,
+                modifier = Modifier.weight(1f),
+                text = "버전 정보",
+                type = TextType.TITLE_5,
                 textAlign = TextAlign.Start,
                 color = JypColors.Text80,
+            )
+            JypText(
+                text = "1.0",
+                type = TextType.BODY_1,
+                textAlign = TextAlign.End,
+                color = JypColors.Text40,
+            )
+        }
+        Spacer(modifier = Modifier.size(12.dp))
+        JypText(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    color = JypColors.Background_white100,
+                    shape = RoundedCornerShape(10.dp)
+                )
+                .padding(
+                    horizontal = 20.dp,
+                    vertical = 23.dp
+                )
+                .clickable(
+                    interactionSource = interactionSource,
+                    indication = null,
+                ) { onClickSignOut() },
+            text = "로그아웃",
+            type = TextType.TITLE_5,
+            textAlign = TextAlign.Start,
+            color = JypColors.Text80,
+        )
+        Spacer(modifier = Modifier.size(12.dp))
+        JypText(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    color = JypColors.Background_white100,
+                    shape = RoundedCornerShape(10.dp)
+                )
+                .padding(
+                    horizontal = 20.dp,
+                    vertical = 23.dp
+                )
+                .clickable(
+                    interactionSource = interactionSource,
+                    indication = null,
+                ) { onClickWithdrawal() },
+            text = "회원 탈퇴",
+            type = TextType.TITLE_5,
+            textAlign = TextAlign.Start,
+            color = JypColors.Text80,
         )
     }
-}
-
-@Composable
-internal fun Divider() {
-    Box(
-            modifier = Modifier
-                    .fillMaxWidth()
-                    .height(1.dp)
-                    .background(Color(0x19000000))
-    )
 }
 
 @Composable
 @Preview
 internal fun MyPageScreenPreview() {
     MyPageScreen(
-            journeyPropensity = "자유로운 탐험가"
+        profileImagePath = "https://file.mk.co.kr/meet/neds/2021/04/image_readtop_2021_330747_16177500644599916.jpg",
+        personality = "자유로운 탐험가",
+        userName = "박진영",
+        onClickAppInfo = {},
+        onClickSignOut = {},
+        onClickWithdrawal = {}
     )
 }
