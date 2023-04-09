67467ec7c7a32ef8ca51a60c46604921195c69fb
yxnsx <yxnsx.dev@gmail.com>
2023-04-09 15:02:37 +0900

    #152: 여행 태그별 선택 유저를 보여주는 바텀시트의 타이틀 값 태그 타입에 따라 설정

--- feature_planner/src/main/java/com/jyp/feature_planner/presentation/planner/TagSelectBottomSheetScreen.kt
+++ feature_planner/src/main/java/com/jyp/feature_planner/presentation/planner/TagSelectBottomSheetScreen.kt
@@ -12,6 +12,7 @@ import com.jyp.feature_planner.domain.PlannerTag
 import com.jyp.jyp_design.resource.JypColors
 import com.jyp.jyp_design.ui.avatar.Avatar
 import com.jyp.jyp_design.ui.tag.DecoratedTag
+import com.jyp.jyp_design.ui.tag.TagType
 import com.jyp.jyp_design.ui.text.JypText
 import com.jyp.jyp_design.ui.typography.type.TextType
 
@@ -27,7 +28,7 @@ internal fun TagSelectedBottomSheetScreen(tag: PlannerTag) {
                             bottom = 40.dp,
                     ),
     ) {
-        Header()
+        Header(tag.type)
         Spacer(modifier = Modifier.size(24.dp))
         DecoratedTag(
                 tagType = tag.type,
@@ -41,14 +42,20 @@ internal fun TagSelectedBottomSheetScreen(tag: PlannerTag) {
 }
 
 @Composable
-private fun Header() {
+private fun Header(
+    tagType: TagType
+) {
     Row(
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically,
     ) {
         JypText(
-                text = "좋아요 태그",
+                text = when (tagType) {
+                    TagType.Soso -> "상관 없어요 태그"
+                    TagType.Like -> "좋아요 태그"
+                    TagType.Dislike -> "싫어요 태그"
+                },
                 type = TextType.TITLE_2,
                 color = JypColors.Text80,
         )
