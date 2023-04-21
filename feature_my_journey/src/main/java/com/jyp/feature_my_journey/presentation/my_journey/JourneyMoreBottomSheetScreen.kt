200f56fcb1922dcf07a88110f775c6d9c67f93fd
yunso <cw2cu96@gmail.com>
2023-04-21 15:34:34 +0900

    #192: JourneyMoreBottomSheetScreen.kt 파일에 자동정렬 적용

--- feature_my_journey/src/main/java/com/jyp/feature_my_journey/presentation/my_journey/JourneyMoreBottomSheetScreen.kt
+++ feature_my_journey/src/main/java/com/jyp/feature_my_journey/presentation/my_journey/JourneyMoreBottomSheetScreen.kt
@@ -14,75 +14,80 @@ import com.jyp.jyp_design.ui.text.JypText
 import com.jyp.jyp_design.ui.typography.type.TextType
 import com.jyp.feature_my_journey.R
 
+
 @Composable
 fun JourneyMoreBottomSheetScreen(
-        journey: Journey,
-        onClickRemove: (Journey) -> Unit,
+    journey: Journey,
+    onClickRemove: (Journey) -> Unit
 ) {
     Column(
-            modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(start = 24.dp, end = 24.dp, top = 28.dp, bottom = 54.dp)
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(
+                start = 24.dp,
+                end = 24.dp,
+                top = 28.dp,
+                bottom = 54.dp
+            )
     ) {
         Header(journey.title)
         Spacer(modifier = Modifier.size(32.dp))
         SelectOptions(
-                options = listOf(
-                        MyJourneyMoreOption(
-                                icon = painterResource(id = R.drawable.ic_delete),
-                                title = "나가기",
-                                onClickOption = {
-                                    onClickRemove.invoke(journey)
-                                }
-                        ),
+            options = listOf(
+                MyJourneyMoreOption(
+                    icon = painterResource(id = R.drawable.ic_delete),
+                    title = "나가기",
+                    onClickOption = { onClickRemove.invoke(journey) }
                 )
+            )
         )
     }
 }
 
 @Composable
 private fun Header(
-        journeyTitle: String,
+    journeyTitle: String
 ) {
     Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
     ) {
         JypText(
-                text = journeyTitle,
-                type = TextType.TITLE_2,
-                color = JypColors.Text80,
+            text = journeyTitle,
+            type = TextType.TITLE_2,
+            color = JypColors.Text80
         )
         JypText(
-                text = "취소",
-                type = TextType.BODY_2,
-                color = JypColors.Text40,
+            text = "취소",
+            type = TextType.BODY_2,
+            color = JypColors.Text40
         )
     }
 }
 
 @Composable
 private fun SelectOptions(
-        options: List<MyJourneyMoreOption>,
+    options: List<MyJourneyMoreOption>
 ) {
     Column(
-            modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier.fillMaxWidth()
     ) {
         options.forEach { option ->
             Row(
-                    modifier = Modifier
-                            .clickable { option.onClickOption.invoke() },
+                modifier = Modifier.clickable {
+                    option.onClickOption.invoke()
+                }
             ) {
                 Image(
-                        painter = option.icon,
-                        contentDescription = null,
+                    painter = option.icon,
+                    contentDescription = null
                 )
                 Spacer(modifier = Modifier.size(15.dp))
                 JypText(
-                        text = option.title,
-                        type = TextType.BODY_1,
-                        color = JypColors.Text75,
+                    text = option.title,
+                    type = TextType.BODY_1,
+                    color = JypColors.Text75
                 )
             }
         }
